d8dddd26cbfd7fff3ef6cc141787d229d6bd7a16
fightorca <82309237+fightorca@users.noreply.github.com>
2021-05-04 06:50:10 +0900

    Set TextInput options

--- App.jsx
+++ App.jsx
@@ -15,7 +15,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="MemoDetail"
+        initialRouteName="SignUp"
         screenOptions={{
           headerStyle: { backgroundColor: '#467FD3' },
           headerTitleStyle: { color: '#ffffff' },
